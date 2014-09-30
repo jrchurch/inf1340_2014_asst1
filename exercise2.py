223c102b3537e5711d44fdea36121b07c729cc61
Sam Novak <sam@server.local>
2014-09-30 13:43:50 -0400

    Added type check, value check, and list
    Exercise 2: added input checks and create list made up of each
    individual input character

--- exercise2.py
+++ exercise2.py
@@ -6,10 +6,9 @@
     Assignment 1, Exercise 2, INF1340 Fall 2014
 """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
+__author__ = 'Sam Novak and Jodie Church'
 
-__copyright__ = "2014 Susan Sim"
+__copyright__ = "2014 Sam Novak and Jodie Church"
 __license__ = "MIT License"
 
 __status__ = "Prototype"
@@ -26,18 +25,22 @@ def checksum (upc):
         Boolean: True, checksum is correct
         False, otherwise
     :raises:
-        TypeError if input is not a strong
+        TypeError if input is not a string
         ValueError if string is the wrong length (with error string stating how many digits are over or under
     """
-
     # check type of input
     # raise TypeError if not string
-
-    # check length of string
-    # raise ValueError if not 12
-
+    if type(upc) is str:
+        # check length of string
+        # raise ValueError if not 12
+        if len(upc) != 12:
+            raise ValueError("UPC must be 12 characters")
+    else:
+        raise TypeError("Invalid type passed as parameter")
     # convert string to array
     # hint: use the list function
+    upc_list = list(upc)
+    print(upc_list)
 
     # generate checksum using the first 11 digits provided
     # check against the the twelfth digit
@@ -46,3 +49,4 @@ def checksum (upc):
 
     return False
 
+checksum('03600029145x')
