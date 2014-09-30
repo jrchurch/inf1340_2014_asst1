9721b2a40988ec481789852013d774503fe43918
Jodie Church <jrchurch@users.noreply.github.com>
2014-09-29 21:31:53 -0400

    Test Commit based on tonight's class
    Using her dictionary example from this evening

--- exercise3.py
+++ exercise3.py
@@ -3,4 +3,18 @@
 
 def decide_rps(player1, player2):
     return 1
+    
+("Rock", "Paper")
+("Paper", "Rock")
+("Scissors", "Paper")
+("Paper", "Scissors")
+
+# rps_results = {}
+# rps_results[("Rock", "Paper")] = 2
+# rps_results["Paper", "Rock"] = 1
+# rps_results["Scissors", "Paper"] = 1
+# rps_results["Paper", "Scissors"] = 2
+
+# print(rps_results["Paper", "Rock"])
+# print(rps_results["Rock", "Paper"])
 
